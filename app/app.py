4f159a76a6b6b47137a1916eb9b27497542e6435
Ricardo Nunes <rn44@njit.edu>
2020-11-21 19:55:37 -0500

    Task: update docker build file.

--- app/app.py
+++ app/app.py
@@ -1,46 +1,123 @@
 from typing import List, Dict
-import mysql.connector
 import simplejson as json
-from flask import Flask, Response
+from flask import Flask, request, Response, redirect
 from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'homeValues'
+mysql.init_app(app)
 
 
-def homeValue_import() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'homeValues'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(dictionary=True)
-
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': 'Home Value Project'}
+    cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM homes')
     result = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    return result
+    return render_template('index.html', title='Home', user=user, homes=result)
 
 
-@app.route('/')
-def index():
-    user = {'username': 'Ricardo'}
-    homes_data = homeValue_import()
+@app.route('/view/<int:home_id>', methods=['GET'])
+def record_view(home_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM homes WHERE id=%s', home_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', homes=result[0])
 
-    return render_template('index.html', title='Home', user=user, homes=homes_data)
+
+@app.route('/edit/<int:home_id>', methods=['GET'])
+def form_edit_get(home_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM homes WHERE id=%s', home_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', homes=result[0])
 
 
-@app.route('/api/homes')
-def cities() -> str:
-    js = json.dumps(homeValue_import())
-    resp = Response(js, status=200, mimetype='application/json')
+@app.route('/edit/<int:home_id>', methods=['POST'])
+def form_update_post(home_id):
+    cursor = mysql.get_db().cursor()
+    inputdata = (request.form.get('Sell'), request.form.get('List'), request.form.get('Living'),
+                 request.form.get('Rooms'), request.form.get('Beds'), request.form.get('Baths'),
+                 request.form.get('Age'), request.form.get('Acres'), request.form.get('Taxes'), home_id)
+    sql_update_query = """UPDATE homes t SET t.Sell = %s, t.List = %s, t.Living = %s, t.Rooms = %s, t.Beds = %s, 
+    t.Age = %s, t.Acres = %s, t.Taxes = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputdata)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/homes/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Home Form')
+
+
+@app.route('/homes/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputdata = (request.form.get('Sell'), request.form.get('List'), request.form.get('Living'),
+                 request.form.get('Rooms'), request.form.get('Beds'), request.form.get('Baths'),
+                 request.form.get('Age'), request.form.get('Acres'), request.form.get('Taxes'), home_id)
+    sql_insert_query = """INSERT INTO homes (Sell,List,Living,Rooms,Beds,Baths,Age,Acres,Taxes) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) """
+    cursor.execute(sql_insert_query, inputdata)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<int:home_id>', methods=['POST'])
+def form_delete_post(home_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM homes WHERE id = %s """
+    cursor.execute(sql_delete_query, home_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/homes', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM homes')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/homes/<int:home_id>', methods=['GET'])
+def api_retrieve(home_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM homes WHERE id=%s', home_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/homes/', methods=['POST'])
+def api_add() -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/homes/<int:home_id>', methods=['PUT'])
+def api_edit(home_id) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/homes/<int:home_id>', methods=['DELETE'])
+def api_delete(home_id) -> str:
+    resp = Response(status=210, mimetype='application/json')
     return resp
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
